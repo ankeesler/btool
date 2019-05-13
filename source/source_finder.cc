85edcd5ca01da603026c608dd8c0c0cea308fb6b
Andrew Keesler <ankeesler1@gmail.com>
2019-05-12 21:14:19 -0400

    Get back to a clean repo.

--- source/source_finder.cc
+++
@@ -1,9 +0,0 @@
-#import "source_finder.h"
-
-#import <string>
-#import <vector>
-
-std::vector<std::string> *SourceFinder::FindAll(void) const {
-  std::vector<std::string> *source_files = new std::vector<std::string>();
-  return source_files;
-}
