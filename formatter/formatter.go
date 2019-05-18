d244c8392dde1187d29d9b1cf8885fd2c701e6c1
Andrew Keesler <ankeesler1@gmail.com>
2019-05-18 00:41:37 -0400

    Wow it kinda works...

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -28,16 +28,16 @@ func (f *formatter) Format(entry *logrus.Entry) ([]byte, error) {
 func colorLevel(level logrus.Level) string {
 	switch level {
 	case logrus.DebugLevel:
-		return color.HiBlackString("%-5s", level)
+		return color.HiBlackString("%5s", level)
 	case logrus.InfoLevel:
-		return color.BlueString("%-5s", level)
+		return color.BlueString("%5s", level)
 	case logrus.WarnLevel:
-		return color.YellowString("%-5s", level)
+		return color.YellowString("%5s", level)
 	case logrus.ErrorLevel:
-		return color.HiRedString("%-5s", level)
+		return color.HiRedString("%5s", level)
 	case logrus.FatalLevel:
-		return color.RedString("%-5s", level)
+		return color.RedString("%5s", level)
 	default:
-		return color.BlackString("%-5s", level)
+		return color.BlackString("%5s", level)
 	}
 }
