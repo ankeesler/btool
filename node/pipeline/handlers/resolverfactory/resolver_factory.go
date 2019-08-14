ab4f02f6ec2efe12dd513cfc014a11d40755c1d9
Andrew Keesler <akeesler@pivotal.io>
2019-08-14 12:17:26 -0400

    Check in a prototype resolver factory.

---
+++ node/pipeline/handlers/resolverfactory/resolver_factory.go
@@ -0,0 +1,67 @@
+// Package resolverfactory provides a factory type for creating node.Resolver's.
+package resolverfactory
+
+import (
+	"github.com/ankeesler/btool/node"
+	"github.com/pkg/errors"
+)
+
+// These are the known node.Resolver name's that can be provided to
+// ResolverFactory.NewResolver(). They should be self explanatory. :)
+const (
+	NameCompile = "compile"
+	NameArchive = "archive"
+	NameLink    = "link"
+
+	NameDownload = "download"
+	NameUnzip    = "unzip"
+)
+
+// These are the known config options for each node.Resolver that can be provided
+// to ResolverFactory.NewResolver() in the config. They should be self
+// explanatory. :)
+const (
+	ConfigCompileIncludePaths = NameCompile + ".includePaths"
+)
+
+// ResolverFactory is a factory type that can create node.Resolver's.
+type ResolverFactory struct {
+	compilerC, compilerCC, archiver, linker string
+}
+
+// NewResolverFactory creates a new ResolverFactory.
+func NewResolverFactory(
+	compilerC, compilerCC, archiver, linker string,
+) *ResolverFactory {
+	return &ResolverFactory{
+		compilerC:  compilerC,
+		compilerCC: compilerCC,
+		archiver:   archiver,
+		linker:     linker,
+	}
+}
+
+// NewResolver creates a new node.Resolver from the provided name and config.
+// The provided name should be one of the Name* constants defined in this
+// package. The provided config should contains key that are one of the Config*
+// constants provided in this package.
+//
+// NewResolver will return an error if it cannot create a node.Resolver for the
+// provided name and config.
+func (rf *ResolverFactory) NewResolver(
+	name string,
+	config map[string]interface{},
+) (node.Resolver, error) {
+	var r node.Resolver
+	var err error
+	switch name {
+	}
+
+	if err != nil {
+		return errors.Wrap(err, "create "+name)
+	} else if r == nil {
+		return errors.New("unknown resolver: " + name)
+	}
+
+	return r, nil
+}
