2358daa13185a7fae30947f5e73ec4432a466395
Andrew Keesler <akeesler@pivotal.io>
2019-08-28 10:45:54 -0400

    Apparently this doesn't work . :(

--- collector/creator.go
+++ collector/creator.go
@@ -2,15 +2,18 @@ package collector
 
 import "github.com/pkg/errors"
 
+// CollectiniCreator is an object that can create a Collectini.
 type CollectiniCreator interface {
 	Create() (Collectini, error)
 }
 
+// Create can create a Collector.
 type Creator struct {
 	ctx    *Ctx
 	cinics []CollectiniCreator
 }
 
+// NewCreator creates a new Creator.
 func NewCreator(ctx *Ctx, cinics []CollectiniCreator) *Creator {
 	return &Creator{
 		ctx:    ctx,
@@ -18,10 +21,12 @@ func NewCreator(ctx *Ctx, cinics []CollectiniCreator) *Creator {
 	}
 }
 
+// Create will create a new Collector, injecting all of the Collectini's created
+// via the CollectiniCreator's.
 func (c *Creator) Create() (*Collector, error) {
 	cinis := make([]Collectini, len(c.cinics))
-	for i := range c.cinics {
-		cini, err := c.cinics[i].Create()
+	for _, cinic := range c.cinics {
+		cini, err := cinic.Create()
 		if err != nil {
 			return nil, errors.Wrap(err, "create")
 		}
--- run.go
+++ run.go
@@ -1,5 +1,21 @@
 package btool
 
+import (
+	"path/filepath"
+
+	"github.com/ankeesler/btool/builder"
+	"github.com/ankeesler/btool/builder/currenter"
+	"github.com/ankeesler/btool/cleaner"
+	"github.com/ankeesler/btool/collector"
+	"github.com/ankeesler/btool/collector/resolverfactory"
+	"github.com/ankeesler/btool/collector/scanner"
+	"github.com/ankeesler/btool/collector/scanner/includeser"
+	"github.com/ankeesler/btool/collector/sorter"
+	"github.com/ankeesler/btool/node"
+	"github.com/ankeesler/btool/ui"
+	"github.com/spf13/afero"
+)
+
 // Cfg is a configuration struct provided to a Build call.
 //
 // Callers should set all fields.
@@ -28,29 +44,30 @@ type Cfg struct {
 // provided Cfg, passes those dependencies to New(), and calls Run() on the
 // returned Btool struct.
 func Run(cfg *Cfg) error {
-	//ui := ui.New(cfg.Quiet)
-	//
-	//fs := afero.NewOsFs()
-	//ns := collector.NewNodeStore(ui)
-	//i := includeser.New(fs)
-	//rf := resolverfactory.New(
-	//	cfg.CompilerC,
-	//	cfg.CompilerCC,
-	//	cfg.Archiver,
-	//	cfg.LinkerC,
-	//	cfg.LinkerCC,
-	//)
-	//scanner := scanner.New(fs, cfg.Root, i)
-	//sorter := sorter.New()
-	//
-	//ctx := collector.NewCtx(ns, rf)
-	//collector := collector.New(ctx, scanner, sorter)
-	//cleaner := cleaner.New(fs, ui)
-	//builder := builder.New(cfg.DryRun, currenter.New(), ui)
-	//
-	//target := filepath.Join(cfg.Root, cfg.Target)
-	//targetN := node.New(target)
-	//b := New(collector, cleaner, builder)
-	//return b.Run(targetN, cfg.Clean, cfg.DryRun)
-	return nil
+	ui := ui.New(cfg.Quiet)
+
+	fs := afero.NewOsFs()
+	ns := collector.NewNodeStore(ui)
+	i := includeser.New(fs)
+	rf := resolverfactory.New(
+		cfg.CompilerC,
+		cfg.CompilerCC,
+		cfg.Archiver,
+		cfg.LinkerC,
+		cfg.LinkerCC,
+	)
+	scanner := scanner.New(fs, cfg.Root, i)
+	sorter := sorter.New()
+
+	ctx := collector.NewCtx(ns, rf)
+	cleaner := cleaner.New(fs, ui)
+	builder := builder.New(cfg.DryRun, currenter.New(), ui)
+
+	cinics := []collector.CollectiniCreator{}
+	cc := collector.NewCreator(ctx, cinics)
+	b := New(cc, cleaner, builder)
+
+	target := filepath.Join(cfg.Root, cfg.Target)
+	targetN := node.New(target)
+	return b.Run(targetN, cfg.Clean, cfg.DryRun)
 }
