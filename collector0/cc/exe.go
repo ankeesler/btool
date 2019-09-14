6bf75f0d95053b19f7195d286108f4f3bf4c9f24
Andrew Keesler <ankeesler1@gmail.com>
2019-09-13 20:06:05 -0400

    collector/cc: add exe.

---
+++ collector0/cc/exe.go
@@ -0,0 +1,89 @@
+package cc
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	collector "github.com/ankeesler/btool/collector0"
+	"github.com/ankeesler/btool/node"
+	"github.com/pkg/errors"
+)
+
+type Exe struct {
+	rf ResolverFactory
+}
+
+func NewExe(rf ResolverFactory) *Exe {
+	return &Exe{
+		rf: rf,
+	}
+}
+
+func (e *Exe) Consume(s collector.Store, n *node.Node) error {
+	if filepath.Ext(n.Name) != "" {
+		return nil
+	}
+
+	c := s.Get(n.Name + ".c")
+	cc := s.Get(n.Name + ".cc")
+	if c != nil && cc != nil {
+		return fmt.Errorf("ambiguous exe %s: %s or %s?", n, c, cc)
+	} else if c == nil && cc == nil {
+		return fmt.Errorf("no known source for exe %s", n)
+	}
+
+	var r node.Resolver
+	var err error
+	objs := make(map[string]*node.Node)
+	if c != nil {
+		err = collectObjs(s, c, ".c", objs)
+		r = e.rf.NewLinkC()
+	} else { // cc
+		err = collectObjs(s, cc, ".cc", objs)
+		r = e.rf.NewLinkCC()
+	}
+
+	if err != nil {
+		return errors.Wrap(err, "collect objs")
+	}
+
+	for _, obj := range objs {
+		n.Dependency(obj)
+	}
+	n.Resolver = r
+
+	s.Set(n)
+
+	return nil
+}
+
+// The provided n should be a c/cc file!
+func collectObjs(
+	s collector.Store,
+	n *node.Node,
+	ext string,
+	objs map[string]*node.Node,
+) error {
+	obj := s.Get(strings.ReplaceAll(n.Name, ext, ".o"))
+	if obj == nil {
+		return fmt.Errorf("no know object for %s", n)
+	}
+	if _, ok := objs[obj.Name]; ok {
+		return nil
+	}
+	objs[obj.Name] = obj
+
+	for _, d := range n.Dependencies {
+		if !strings.HasSuffix(d.Name, ".h") {
+			continue
+		}
+
+		src := s.Get(strings.ReplaceAll(d.Name, ".h", ext))
+		if err := collectObjs(s, src, ext, objs); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ collector0/cc/exe_test.go
@@ -0,0 +1,78 @@
+package cc_test
+
+import (
+	"testing"
+
+	"github.com/ankeesler/btool/collector0/cc"
+	"github.com/ankeesler/btool/collector0/cc/ccfakes"
+	"github.com/ankeesler/btool/collector0/testutil"
+	"github.com/ankeesler/btool/node"
+	"github.com/ankeesler/btool/node/nodefakes"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExe(t *testing.T) {
+	ah := node.New("a/a.h")
+	ac := node.New("a/a.c").Dependency(ah)
+	acc := node.New("a/a.cc").Dependency(ah)
+	ao := node.New("a/a.o").Dependency(ac)
+	bh := node.New("b/b.h").Dependency(ah)
+	bc := node.New("b/b.c").Dependency(ah, bh)
+	bcc := node.New("b/b.cc").Dependency(ah, bh)
+	bo := node.New("b/b.o").Dependency(bc)
+	mainc := node.New("main.c").Dependency(bh)
+	maincc := node.New("main.cc").Dependency(bh)
+	maino := node.New("main.o").Dependency(mainc)
+
+	t.Run("C", func(t *testing.T) {
+		r := &nodefakes.FakeResolver{}
+
+		rf := &ccfakes.FakeResolverFactory{}
+		rf.NewLinkCReturnsOnCall(0, r)
+
+		e := cc.NewExe(rf)
+
+		s := testutil.FakeStore(maino, mainc, ao, ac, ah, bo, bc, bh)
+
+		main := node.New("main")
+		require.Nil(t, e.Consume(s, main))
+
+		assert.Equal(t, 1, rf.NewLinkCCallCount())
+
+		assert.Equal(t, 1, s.SetCallCount())
+		exMain := node.New("main").Dependency(maino, bo, ao)
+		exMain.Resolver = r
+		assert.Equal(t, exMain, s.SetArgsForCall(0))
+	})
+
+	t.Run("CC", func(t *testing.T) {
+		r := &nodefakes.FakeResolver{}
+
+		rf := &ccfakes.FakeResolverFactory{}
+		rf.NewLinkCCReturnsOnCall(0, r)
+
+		e := cc.NewExe(rf)
+
+		s := testutil.FakeStore(maino, maincc, ao, acc, ah, bo, bcc, bh)
+
+		main := node.New("main")
+		require.Nil(t, e.Consume(s, main))
+
+		assert.Equal(t, 1, rf.NewLinkCCCallCount())
+
+		assert.Equal(t, 1, s.SetCallCount())
+		exMain := node.New("main").Dependency(maino, bo, ao)
+		exMain.Resolver = r
+		assert.Equal(t, exMain, s.SetArgsForCall(0))
+	})
+
+	t.Run("Noop", func(t *testing.T) {
+		rf := &ccfakes.FakeResolverFactory{}
+		e := cc.NewExe(rf)
+		s := testutil.FakeStore()
+		require.Nil(t, e.Consume(s, node.New("main.c")))
+		assert.Equal(t, 0, rf.NewLinkCCallCount())
+		assert.Equal(t, 0, s.SetCallCount())
+	})
+}
