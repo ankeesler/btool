6510cc06b60e138002e06e4243b21d1a152f61ba
Andrew Keesler <ankeesler1@gmail.com>
2019-09-22 12:37:01 -0400

    app/collector/repository: implement repository producer without caching.

--- app/collector/repository/repository.go
+++ app/collector/repository/repository.go
@@ -3,16 +3,21 @@
 package repository
 
 import (
+	"context"
+	"path/filepath"
+	"time"
+
 	"github.com/ankeesler/btool/app/collector"
 	"github.com/ankeesler/btool/node"
 	nodev1 "github.com/ankeesler/btool/node/api/v1"
+	"github.com/pkg/errors"
 	"github.com/spf13/afero"
 )
 
 //go:generate go run github.com/maxbrunsfeld/counterfeiter/v6 . Unmarshaler
 
-// Unmarshaler is a type that, given a Node and its Repository's revision from
-// the NodeRepository API, can produce a node.Node.
+// Unmarshaler is a type that, given a Node and a root for the Node, can produce
+// a node.Node.
 type Unmarshaler interface {
 	Unmarshal(collector.Store, *nodev1.Node, string) (*node.Node, error)
 }
@@ -43,5 +48,39 @@ func New(
 }
 
 func (r *Repository) Produce(s collector.Store) error {
+	repositories, err := r.listRepositories()
+	if err != nil {
+		return errors.Wrap(err, "list repositories")
+	}
+
+	for _, repository := range repositories {
+		root := filepath.Join(r.cache, repository.Revision)
+
+		// TODO: caching?
+
+		for _, repositoryNode := range repository.Nodes {
+			n, err := r.u.Unmarshal(s, repositoryNode, root)
+			if err != nil {
+				return errors.Wrap(err, "unmarshal")
+			}
+
+			s.Set(n)
+		}
+	}
+
 	return nil
+}
+
+func (r *Repository) listRepositories() ([]*nodev1.Repository, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	req := nodev1.ListRepositoriesRequest{}
+
+	rsp, err := r.c.ListRepositories(ctx, &req)
+	if err != nil {
+		return nil, errors.Wrap(err, "list repositories")
+	}
+
+	return rsp.Repositories, nil
 }
