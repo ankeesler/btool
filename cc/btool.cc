91a5e9622f918e23ac8070bed0adaaf85937b68f
Andrew Keesler <ankeesler1@gmail.com>
2019-11-23 14:57:34 -0500

    Add main function changes for registry stuff.
    We cannot uncomment this until c++ btool supports installing external
    dependencies (yaml) since we depend on building btool in the integration
    tests.

--- cc/btool.cc
+++ cc/btool.cc
@@ -1,6 +1,8 @@
 #include <cstdlib>
 
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -16,6 +18,12 @@
 #include "app/collector/cc/resolver_factory_impl.h"
 #include "app/collector/collector.h"
 #include "app/collector/fs/fs_collectini.h"
+//#include "app/collector/registry/fs_registry.h"
+//#include "app/collector/registry/gaggle_collector_impl.h"
+//#include "app/collector/registry/http_registry.h"
+//#include "app/collector/registry/registry_collectini.h"
+//#include "app/collector/registry/resolver_factory_impl.h"
+// yaml_serializer.h
 #include "app/collector/store.h"
 #include "app/collector/trivial_collectini.h"
 #include "app/lister/lister.h"
@@ -25,6 +33,7 @@
 #include "ui/ui.h"
 #include "util/flags.h"
 #include "util/fs/fs.h"
+#include "util/string/string.h"
 
 // workaround for bug-02
 #include "app/collector/base_collectini.h"
@@ -47,6 +56,8 @@ static const char *linker_cc = "clang++";
 #error "unknown platform"
 #endif
 
+static std::string GetDefaultCache();
+
 int main(int argc, const char *argv[]) {
   ::btool::util::Flags f;
 
@@ -60,6 +71,11 @@ int main(int argc, const char *argv[]) {
   f.String("root", &root);
   std::string target = "main";
   f.String("target", &target);
+  std::string cache = GetDefaultCache();
+  f.String("cache", &cache);
+
+  std::string registry = "https://btoolregistry.cfapps.io";
+  f.String("registry", &registry);
 
   bool clean = false;
   f.Bool("clean", &clean);
@@ -87,23 +103,39 @@ int main(int argc, const char *argv[]) {
 
   ::btool::ui::UI ui;
 
+  ::btool::app::collector::Store s;
+
+  //::btool::app::collector::registry::YamlSerializer ys;
+  //::btool::app::collector::registry::FsRegistry fr(registry, &ys);
+  //::btool::app::collector::registry::HttpRegistry hr(registry, &ys);
+  //::btool::app::collector::registry::Registry *r;
+  // if (::btool::util::string::HasPrefix(registry, "http")) {
+  //  r = &hr;
+  //} else {
+  //  r = &fr;
+  //}
+
+  //::btool::app::collector::registry::ResolverFactoryImpl r_rfi;
+  //::btool::app::collector::registry::GaggleCollectorImpl gci(&r_rfi);
+  //::btool::app::collector::registry::RegistryCollectini rc(r, cache, &gci);
+
   ::btool::app::collector::cc::IncludesParserImpl ipi;
   ::btool::app::collector::cc::Inc i(&ipi);
 
-  ::btool::app::collector::cc::ResolverFactoryImpl rfi(
+  ::btool::app::collector::cc::ResolverFactoryImpl c_rfi(
       compiler_c, compiler_cc, archiver, linker_c, linker_cc,
       {"-Wall", "-Werror", "-g", "-O0", "--std=c17"},
       {"-Wall", "-Werror", "-g", "-O0", "--std=c++17"});
-  ::btool::app::collector::cc::Obj o(&rfi);
-  ::btool::app::collector::cc::Exe e(&rfi);
+  ::btool::app::collector::cc::Obj o(&c_rfi);
+  ::btool::app::collector::cc::Exe e(&c_rfi);
 
   ::btool::app::collector::fs::FSCollectini fsc(root.c_str());
 
   auto root_target = ::btool::util::fs::Join(root, target);
   ::btool::app::collector::TrivialCollectini tc(root_target);
 
-  ::btool::app::collector::Store s;
   ::btool::app::collector::Collector collector(&s);
+  // collector.AddCollectini(&rc);
   collector.AddCollectini(&i);
   collector.AddCollectini(&o);
   collector.AddCollectini(&e);
@@ -123,10 +155,15 @@ int main(int argc, const char *argv[]) {
   ::btool::app::App a(&collector, &cleaner, &lister, &builder, &runner);
   try {
     a.Run(root_target, clean, list, false);
-  } catch (const ::btool::Err &e) {
+  } catch (const std::exception &e) {
     ERRORS() << e.what() << std::endl;
     return 1;
   }
 
   return 0;
+}
+
+static std::string GetDefaultCache() {
+  auto home = ::getenv("HOME");
+  return (home == nullptr ? ".btool" : ::btool::util::fs::Join(home, ".btool"));
 }
