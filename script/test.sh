cee7f11ece814c48e917758489177a3348e239d0
Andrew Keesler <ankeesler1@gmail.com>
2019-11-27 23:07:07 -0500

    Move integration tests to root directory.

--- script/test.sh
+++ script/test.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
-set -eou pipefail
+set -eo pipefail
+
+if [[ -z "$BTOOL" ]]; then
+  BTOOL="$(which btool)"
+fi 
+
+if [[ -z "$REGISTRY" ]]; then
+  REGISTRY="https://btoolregistry.cfapps.io"
+fi 
+
+run_test() {
+  "$BTOOL" -root source -registry "$REGISTRY" -loglevel error -run -target "$1"
+}
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
-./source/script/test.sh
+
+tests="$(find . -name "*_test.cc" | sed -e 's!./source/!!;s!\.cc!!')"
+for t in $tests; do
+  if [[ "$t" == "util/unzip_test" ]]; then
+    echo
+    echo "**********"
+    echo
+    echo "skipping util/unzip_test - libzip does not work on linux"
+    echo
+    echo "**********"
+    echo
+  else
+    run_test "$t"
+  fi
+done
+
+python integration/integration.py \
+       "$BTOOL" /tmp/btool-new "$REGISTRY"
+python integration/integration.py \
+       /tmp/btool-new /tmp/btool-new-new "$REGISTRY"
