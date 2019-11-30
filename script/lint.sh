72038aaadf163df0b773066f0d108d8c0ecb9ecf
Andrew Keesler <ankeesler1@gmail.com>
2019-11-29 23:33:56 -0500

    ci: check that files are linted.

--- script/lint.sh
+++ script/lint.sh
@@ -2,10 +2,40 @@
 
 set -eo pipefail
 
+install_clang_format_brew() {
+  brew install clang-format
+}
+
+install_clang_format_linux() {
+  apt-get update && apt-get install clang-format
+}
+
+install_clang_format() {
+    case "$(uname)" in
+      Darwin)
+        install_clang_format_darwin
+        ;;
+      Linux)
+        install_clang_format_linux
+        ;;
+    esac
+}
+
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+if ! which clang-format >/dev/null; then
+  install_clang_format
+fi
+
 files="$(find . -name "*.h" -or -name "*.cc")"
+diff_exists=0
 for file in $files; do
   clang-format -style=Google "$file" > /tmp/btool-lint-tmp
+  if ! diff /tmp/btool-lint-tmp "$file" >/dev/null; then
+     diff_exists=1
+  fi
   mv /tmp/btool-lint-tmp "$file"
   echo "$file"
 done
+
+exit "$diff_exists"
