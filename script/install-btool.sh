1aa02c58ba0453ac6eb124f482abb744f1296671
Andrew Keesler <ankeesler1@gmail.com>
2019-12-09 12:47:35 -0500

    script: install-btool.sh can install latest and local versions.
    This also updates CI to install the latest version of btool in ankeesler/btool
    every time CI passes. I think this will make lives easier when we release breaking
    features as we move through version 0 status.

--- script/install-btool.sh
+++ script/install-btool.sh
@@ -2,16 +2,53 @@
 
 set -eo pipefail
 
-VERSION="0.4"
-OS="linux"
+usage() {
+  echo "usage: install-btool.sh local        (1)"
+  echo "usage: install-btool.sh latest       (2)"
+  echo "usage: install-btool.sh <version>    (3)"
+  echo
+  echo "(1) build locally (requires local btool binary"
+  echo "(2) download latest released btool binary"
+  echo "(3) download released btool binary with verison <version>"
+}
 
-if [[ "$(uname)" == "Darwin" ]]; then
-  OS="macos"
+download_and_install() {
+  version="$1"
+  os="$2"
+  curl \
+    -L "https://github.com/ankeesler/btool/releases/download/$version/btool-$version-$os-x86_64.gz" \
+    -o - \
+    -f \
+    | gunzip \
+    > /usr/local/bin/btool \
+    && chmod +x /usr/local/bin/btool
+}
+
+version="$1"
+if [[ -z "$version" ]]; then
+  usage
+  exit 1
 fi
 
-curl \
-  -L "https://github.com/ankeesler/btool/releases/download/$VERSION/btool-$VERSION-$OS-x86_64.gz" \
-  -o - \
-  | gunzip \
-  > /usr/local/bin/btool \
-  && chmod +x /usr/local/bin/btool
+os="linux"
+if [[ "$(uname)" == "Darwin" ]]; then
+  os="macos"
+fi
+
+echo "install-btool.sh: installing version $version"
+if [[ "$version" == "local" ]]; then
+  if ! which btool; then
+    echo "install-btool.sh: error: no local btool binary"
+    exit 1
+  fi
+
+  btool -root source -target btool -clean # TODO: remove me when builds work across os's!
+  btool -root source -target btool
+  cp source/btool /usr/local/bin/btool
+elif [[ "$version" == "latest" ]]; then
+  version="$(curl -f https://api.github.com/repos/ankeesler/btool/releases/latest | jq -r .tag_name)"
+  download_and_install "$version" "$os"
+else
+  download_and_install "$version" "$os"
+fi
+
